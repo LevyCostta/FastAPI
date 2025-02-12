221e29f5f12fd4d3fae386f6ffe45571da42f0d6
LevyCostta <levicostasouza@hotmail.com>
2025-02-12 20:31:54 -0300

    Iniciando Projeto

---
+++ main.py
@@ -0,0 +1,14 @@
+import uvicorn
+from fastapi import FastAPI
+from contas_a_pagar_e_receber.router import contas_a_pagar_e_receber_router
+
+app = FastAPI()
+
+@app.get('/')
+def sou_programador() -> str:
+    return 'Oi, eu sou um programador!'
+
+app.include_router(contas_a_pagar_e_receber_router.router)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='0.0.0.0', port=8001)
