95e7dac7ac492747550f388b3865b20e1888da37
LevyCostta <levicostasouza@hotmail.com>
2025-02-12 21:11:43 -0300

    Adicionado o test criar contas

--- contas_a_pagar_e_receber/test/contas_a_pagar_e_receber/routers/test_contas_a_pagar_e_receber_router.py
+++ contas_a_pagar_e_receber/test/contas_a_pagar_e_receber/routers/test_contas_a_pagar_e_receber_router.py
@@ -12,3 +12,15 @@ def test_deve_listar_contas_a_pagar_e_receber():
         {'id': 2, 'descricao': 'Salario', 'valor': '5000', 'tipo': 'RECEBER'}
         ]
 
+def test_deve_criar_contas_a_pagar_e_receber():
+    nova_conta = {
+        'descricao': 'Curso de Python',
+        'valor': '500',
+        'tipo': 'PAGAR'
+    }
+    nova_conta_copy = nova_conta.copy()
+    nova_conta_copy['id'] = 3
+
+    response = client.post('/contas-a-pagar-e-receber/',json=nova_conta)
+    assert response.status_code == 201
+    assert response.json() == nova_conta_copy
